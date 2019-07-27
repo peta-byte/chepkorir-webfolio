6a2cdc1371094e9fb62f64ab87131af712b3d324
Chepkorir Brenda <40006302+ChepkorirBrenda@users.noreply.github.com>
2019-07-27 15:40:10 -0500

    Add files via upload

---
+++ src/app/profile/profile.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "profile",
+  templateUrl: "./profile.component.html",
+  styleUrls: ["./profile-styles.component.css"]
+})
+export class ProfileComponent {}
