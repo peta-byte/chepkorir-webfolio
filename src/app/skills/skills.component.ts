05162a074c22333f6c62b4da61da4f573f456ec0
Chepkorir Brenda <40006302+ChepkorirBrenda@users.noreply.github.com>
2019-07-27 15:38:41 -0500

    Add files via upload

---
+++ src/app/skills/skills.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "skills",
+  templateUrl: "./skills.component.html",
+  styleUrls: ["./skills.component.css"]
+})
+export class SkillsComponent {}
