2d4dc70cad89e169ea8636b19cf95d5d38780d59
Chepkorir Brenda <40006302+ChepkorirBrenda@users.noreply.github.com>
2019-07-27 15:32:55 -0500

    Add files via upload

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { nextAnimation } from "../app.animations";
 
 @Component({
-  selector: 'projects-root',
-  templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.css']
+  selector: "projects-root",
+  templateUrl: "./projects.component.html",
+  styleUrls: ["./projects.component.css"],
+  animations: [nextAnimation]
 })
-export class ProjectsComponent {
-
+export class ProjectsComponent implements OnInit {
+  state: string = "in";
+  ngOnInit(): void {
+    this.state = this.state === "in" ? "out" : "in";
+  }
 }
