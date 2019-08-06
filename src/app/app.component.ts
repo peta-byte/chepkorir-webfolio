5ffda8e16515b967573dc2ec106bb62015397b6c
Chepkorir Brenda <40006302+ChepkorirBrenda@users.noreply.github.com>
2019-08-06 18:36:48 -0500

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { Router, NavigationEnd } from "@angular/router";
+import { timer } from "rxjs";
+declare var $: any;
 
 @Component({
   selector: "app-root",
@@ -8,6 +10,8 @@ import { Router, NavigationEnd } from "@angular/router";
 })
 export class AppComponent implements OnInit {
   headerTitle: string = "Projects";
+  // modal will pop up after 2 minutes on the site
+  modalTime: number = 120000;
   constructor(private router: Router) {}
   ngOnInit(): void {
     this.router.events.subscribe(value => {
@@ -21,5 +25,9 @@ export class AppComponent implements OnInit {
       	  }
         }
     });
+    const source = timer(this.modalTime);
+        source.subscribe(val => {
+          $('#resume-modal').modal();
+    })
   }
 }
